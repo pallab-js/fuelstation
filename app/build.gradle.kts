96698a0b7a20da504996153eaf93bda14d7427bd
pallab-js <sonowalpallabjyoti@gmail.com>
2026-05-02 23:30:15 +0530

    fix: revert to kotlinOptions (compilerOptions not available in android block with AGP 8.4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    kotlinOptions {
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
